fa2781ed2fa5dec9e0e103ea42a2dcd166cab5fb
Nick Smith <nicksmith@monzo.com>
2025-12-29 11:06:53 +0000

    Better comment

--- osquery/tables/yara/yara.cpp
+++ osquery/tables/yara/yara.cpp
@@ -52,7 +52,7 @@ FLAG(uint32,
      "Time in ms to sleep after scan of each file (default 50) to reduce "
      "memory spikes");
 
-// TODO: in a future major version, set the default value to a sane default to prevent yara
+// TODO: in a future major version, set the default to a sane value to prevent yara
 // scans blocking other processing until the process is restarted. A sane value might be 300 seconds/5 minutes.
 FLAG(int32,
      yara_timeout,
